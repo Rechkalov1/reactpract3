9eeb4dbe29307365b71fd3aafc3ec323dab77d8f
EnVy <envy7321@gmail.com>
2022-09-12 21:35:55 +0300

    save

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,11 @@
 import { PageTitle } from "components/PageTitle/PageTitle"
 import { EventBoard } from "./EventBoard/EventBoard"
+import {Counter} from './Counter/Counter'
 import upcomingEvents from '../upcoming-events.json'
 export const App = () => {
     return <div>
        <PageTitle text="24TH CORE WORLDS COALITION CONFERENCE"/>
        <EventBoard events={upcomingEvents}/>
+       <Counter/>
        </div>
 }
---
+++ src/components/Counter/Counter.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+class Counter extends React.Component{
+    render() {
+return(
+    <div>
+        <span>0</span>
+        <div>
+            <button type="button"onClick={
+                ()=>{
+                    console.log("Клик увеличить")
+                }
+            }>Увеличить на 1</button>
+            <button type="button">Уменьшить на 1</button>
+        </div>
+    </div>
+)
+    }
+}
+
+
+export default Counter
